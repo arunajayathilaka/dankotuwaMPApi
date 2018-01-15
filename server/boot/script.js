da4a42b23ccba339f466b774d423cd7f345b7c10
arunadj <arunadilshanjayathilake@yahoo.com>
2018-01-15 08:10:11 +0530

    remove user already exit

--- server/boot/script.js
+++ server/boot/script.js
@@ -3,25 +3,25 @@ module.exports = function(app) {
 var User = app.models.user;
 var Role = app.models.Role;
 var RoleMapping = app.models.RoleMapping;
-
-User.create([
-    {username: 'aruna', email: 'aruna@admin.com', password: 'aruna'}
-], function(err, users) {
-    if (err) throw err;
+var userDetail = {username: 'aruna', email: 'aruna@admin.com', password: 'aruna'};
+//findOrCreate({where: {email: user.email}}, user, function(err, account) {
+User.findOrCreate(
+    {where: {username: 'aruna', email: 'aruna@admin.com'}},userDetail, function(err, users) {
+    if (err) {console.log(err);};
     //...
     // Create projects, assign project owners and project team members
     //...
     // Create the admin role
-    Role.create({
+    Role.findOrCreate({where:{
       name: 'admin'
-    }, function(err, role) {
+    }},{name: 'admin'}, function(err, role) {
       if (err) throw err;
       //debug(role);
 
       // Make Bob an admin
       role.principals.create({
         principalType: RoleMapping.USER,
-        principalId: users[0].id
+        principalId: users.id
       }, function(err, principal) {
         if (err) throw err;
         //debug(principal);
